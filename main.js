ae3bee00d1df55929633aa415d082848cdc1499a
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2023-02-17 13:43:33 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+nose_x = 0;
+nose_y = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(650,550);
@@ -14,9 +20,29 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("left wrist X =  "+leftWristX+" right wrist X = "+rightWristX);
+
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("Nose x = "+nose_x+" Nose y = "+nose_y);
+
+        difference = floor(leftWristX-rightWristX);
+        console.log("difference = "+difference);
+    }
+    else{
+        console.error(error);
     }
 }
  
-function draw(){}
+function draw(){
+    background("#C3C8CA");
+    textSize(difference);
+    fill("blue");
+    text("Sanyam is the best",nose_x,nose_y);
+}
 
-function modelLoaded(){}
+function modelLoaded(){
+console.log("model loaded!")
+}
